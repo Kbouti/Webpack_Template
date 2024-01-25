b08003c799ca0d32fc011ef7b553ce5686b11121
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-25 15:08:22 -0800

    Organize index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
-import _ from 'lodash';
-
-import './style.css'
-
-import Dice from './dice.png'
-
-import { boardLogicReportToDom,
-boardLogicReportToConsole } from './boardLogic';
+import _ from "lodash";
+import "./style.css";
+import Dice from "./dice.png";
+import { boardLogicReportToDom, boardLogicReportToConsole } from "./boardLogic";
 
 function component() {
-    const element = document.createElement('div');
-    // Lodash, Now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add(`hello`);
+  const element = document.createElement("div");
+  // Lodash, Now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add(`hello`);
+  const myIcon = new Image();
+  myIcon.src = Dice;
+  element.appendChild(myIcon);
+  return element;
+}
+document.body.appendChild(component());
 
-    const myIcon = new Image();
-    myIcon.src = Dice;
-    element.appendChild(myIcon);    
+boardLogicReportToDom();
+boardLogicReportToConsole();
 
-
-    return element;
-  }
-  document.body.appendChild(component());
-
-  boardLogicReportToDom();
-  boardLogicReportToConsole();
-
-
-
-
-
-
-  // Fantastic wepack tutorial: https://www.youtube.com/watch?v=IZGNcSuwBZs
+// Fantastic wepack tutorial: https://www.youtube.com/watch?v=IZGNcSuwBZs
